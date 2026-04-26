28354c3ef16115d9b69a7c97c257b03a106ea8e5
dano-4 <bendradani@gmail.com>
2026-04-26 14:33:18 +0200

    Added another Task Method to fetch all cards from a single set

--- backend/OneSake.Server/Services/CardService.cs
+++ backend/OneSake.Server/Services/CardService.cs
@@ -13,14 +13,53 @@ public class CardService
         _http = http;
     }
 
+    private readonly Dictionary<string, List<CardDto>> _cardCache = new(); // Dictionary mit String als key und liste von den karten da es mehrere karten gibt per person zb. Zoro Parallel und Zoro Normal fuer zb. OP01-001
+    private readonly Dictionary<string, List<CardDto>> _setCache = new(); // Dictionary mit key um ganze sets zu bekommen, bruachbar um Deckbuilder zu machen
+
     // Holt Karten von der externen API
     public async Task<List<CardDto>> GetCardsByIdAsync(string id)
     {
+        if (_cardCache.ContainsKey(id)) return _cardCache[id];
         // API Endpoint
         var url = $"https://optcgapi.com/api/sets/card/{id}/";
 
-        var cards = await _http.GetFromJsonAsync<List<CardDto>>(url);
+        try
+        {
+            var cards = await _http.GetFromJsonAsync<List<CardDto>>(url) ?? new List<CardDto>();
 
-        return cards ?? new List<CardDto>();
+            // Storing Cache
+            _cardCache[id] = cards;
+
+            return cards;
+        }
+
+        catch
+        {
+            return new List<CardDto>();
+        }
     }
+
+    public async Task<List<CardDto>> GetSetByIdAsync(string set_id)
+    {
+        if (_setCache.ContainsKey(set_id)) return _setCache[set_id];
+
+        // API Endpoint
+        var url = $"https://www.optcgapi.com/api/sets/{set_id}/";
+
+        try
+        {
+            var set = await _http.GetFromJsonAsync<List<CardDto>>(url) ?? new List<CardDto>();
+
+            _setCache[set_id] = set;
+
+            return set;
+        }
+        catch
+        {
+            return new List<CardDto>();
+        }
+    }
+
 }
+
+
