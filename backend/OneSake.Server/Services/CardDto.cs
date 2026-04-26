46b7d5b22961c2a54986e4d1a2627ced9f715f6e
dano-4 <bendradani@gmail.com>
2026-04-26 13:40:53 +0200

    Extended the CardDto Class to the full values of the cards

--- backend/OneSake.Server/Services/CardDto.cs
+++ backend/OneSake.Server/Services/CardDto.cs
@@ -9,8 +9,17 @@ namespace OneSake.Server.Services
         [JsonPropertyName("card_id")]
         public string Id { get; set; } = "";
 
+        [JsonPropertyName("card_code")]
+        public string Code { get; set; } = "";
+
         [JsonPropertyName("card_name")]
         public string Name { get; set; } = "";
+
+        [JsonPropertyName("card_setcode")]
+        public string SetCode { get; set; } = "";
+
+        [JsonPropertyName("card_rarity")]
+        public string Rarity { get; set; } = "";
 
         [JsonPropertyName("card_type")]
         public string Type { get; set; } = "";
@@ -24,11 +33,20 @@ namespace OneSake.Server.Services
         [JsonPropertyName("card_power")]
         public string Power { get; set; } = "";
 
-        [JsonPropertyName("card_text")]
-        public string Text { get; set; } = "";
+        [JsonPropertyName("card_counterval")]
+        public string CounterVal { get; set; } = "";
 
-        [JsonPropertyName("card_image")]
+        [JsonPropertyName("card_attribute")]
+        public string Attribute { get; set; } = "";
+
+        [JsonPropertyName("card_life")]
+        public int? Life { get; set; }
+
+        [JsonPropertyName("card_imageurl")]
         public string ImageUrl { get; set; } = "";
+
+        [JsonPropertyName("card_createdat")]
+        public DateTime CreatedAt { get; set; }
 
 
         // Ohne [JsonPropertyName] kann deserialization verkacken
