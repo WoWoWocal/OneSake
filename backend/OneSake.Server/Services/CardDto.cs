f86c00d7da6c1b1f85b0c51997cb184fa8e4853b
dano-4 <bendradani@gmail.com>
2026-04-26 14:22:22 +0200

    Changed JsonPropertyNames to the actual ones of the Api

--- backend/OneSake.Server/Services/CardDto.cs
+++ backend/OneSake.Server/Services/CardDto.cs
@@ -6,47 +6,64 @@ namespace OneSake.Server.Services
     // CardService existiert um uns zu zeigen welche Art von Karten es gibt, restlichen Game-Side Dinge werden dann im .Game Ordner erledigt : Rules, Effects, etc...
     public class CardDto
     {
-        [JsonPropertyName("card_id")]
-        public string Id { get; set; } = "";
+        [JsonPropertyName("inventory_price")]
+        public float? InventoryPrice { get; set; }
 
-        [JsonPropertyName("card_code")]
-        public string Code { get; set; } = "";
+        [JsonPropertyName("market_price")]
+        public float? MarketPrice { get; set; }
 
         [JsonPropertyName("card_name")]
-        public string Name { get; set; } = "";
+        public string CardName { get; set; } = "";
 
-        [JsonPropertyName("card_setcode")]
-        public string SetCode { get; set; } = "";
+        [JsonPropertyName("set_name")]
+        public string SetName { get; set; } = "";
 
-        [JsonPropertyName("card_rarity")]
+        [JsonPropertyName("card_text")]
+        public string CardText { get; set; } = "";
+
+        [JsonPropertyName("set_id")]
+        public string SetId { get; set; } = "";
+
+        [JsonPropertyName("rarity")]
         public string Rarity { get; set; } = "";
 
-        [JsonPropertyName("card_type")]
-        public string Type { get; set; } = "";
+        [JsonPropertyName("card_set_id")]
+        public string CardId { get; set; } = "";
 
         [JsonPropertyName("card_color")]
         public string Color { get; set; } = "";
 
-        [JsonPropertyName("card_cost")]
-        public int? Cost { get; set; }
+        [JsonPropertyName("card_type")]
+        public string Type { get; set; } = "";
 
-        [JsonPropertyName("card_power")]
-        public string Power { get; set; } = "";
-
-        [JsonPropertyName("card_counterval")]
-        public string CounterVal { get; set; } = "";
-
-        [JsonPropertyName("card_attribute")]
-        public string Attribute { get; set; } = "";
-
-        [JsonPropertyName("card_life")]
+        [JsonPropertyName("life")]
         public int? Life { get; set; }
 
-        [JsonPropertyName("card_imageurl")]
-        public string ImageUrl { get; set; } = "";
+        [JsonPropertyName("card_cost")]
+        public int? CardCost { get; set; }
 
-        [JsonPropertyName("card_createdat")]
-        public DateTime CreatedAt { get; set; }
+        [JsonPropertyName("card_power")]
+        public int? CardPower { get; set; }
+
+        [JsonPropertyName("sub_types")]
+        public string SubTypes { get; set; } = "";
+
+        [JsonPropertyName("counter_amount")]
+        public int? CounterAmount { get; set; }
+
+        [JsonPropertyName("attribute")]
+        public string Attribute { get; set; } = "";
+
+        [JsonPropertyName("date_scraped")]
+        public DateTime DateScraped { get; set; }
+
+        [JsonPropertyName("card_image_id")]
+        public string CardImageId { get; set; } = "";
+
+        [JsonPropertyName("card_image")]
+        public string ImageURL { get; set; } = "";
+
+       
 
 
         // Ohne [JsonPropertyName] kann deserialization verkacken
